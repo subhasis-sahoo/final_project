bc88db323ec434071c51c9bbc8e11e14c19f3a03
subhasis-sahoo <subhasissahoo949@gmail.com>
2025-04-04 00:02:06 +0530

    add getSemester and getSubjects function

--- functions.php
+++ functions.php
@@ -1,14 +1,36 @@
 <?php
     require_once "dbconnect.php";
 
-    function getSubjects($semester) {
+    // Get student's semester from the students table
+    function getSemester($sic) {
         $conn = getConnection();
         // print_r($conn);
 
         try {
+            $qry = "SELECT semester FROM students WHERE sic=?";
+            $stmt = $conn->prepare($qry);
+            $stmt->bind_param("s", $sic);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            
+            return $result;
+
+        } catch(Exception $e) {
+            $e->getMessage();
+        } finally {
+            $conn->close();
+        }
+
+    }
+
+    // Getting all the subjects list from the subjects table according to the semester
+    function getSubjects($searched_semester) {
+        $conn = getConnection();
+
+        try {
             $qry = "SELECT subject_code, subject_name FROM subjects WHERE semester=?";
             $stmt = $conn->prepare($qry);
-            $stmt->bind_param("i", $semester);
+            $stmt->bind_param("i", $searched_semester);
             $stmt->execute();
             $result = $stmt->get_result();
 
